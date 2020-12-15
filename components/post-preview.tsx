4d1b886c01cf21b0267cc7a685a91d8464b0e22e
Ludovic Lacouture <lacoludo@outlook.com>
2020-12-15 02:33:37 +0100

    Avatar removed

--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -35,7 +35,6 @@ const PostPreview = ({
         <DateFormatter dateString={date} />
       </div>
       <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-      <Avatar name={author.name} picture={author.picture} />
     </div>
   )
 }
