3c62b3cf1f44c20e2ddeb3344a3e91e8f6b6a614
Ludovic Lacouture <lacoludo@outlook.com>
2020-12-15 15:13:48 +0100

    Context menu disabled

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import '@assets/main.css'
 
+import { useEffect } from 'react'
+
 import type { AppProps } from 'next/app'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(
+    () => document.addEventListener('contextmenu', e => e.preventDefault()),
+    []
+  )
+
   return <Component {...pageProps} />
 }
